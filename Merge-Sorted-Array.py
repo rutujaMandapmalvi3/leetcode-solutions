861d33ec8789495cc46e64b9960f8b3e13908bde
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-05-25 14:25:31 -0700

    [88] [Time Beats: 100.00%] [Memory Beats: 99.46%] - LeetPush

--- Merge-Sorted-Array.py
+++ Merge-Sorted-Array.py
@@ -1,24 +1,19 @@
-'''
-Time complexity = O(m+n)
-Space complexity = O(1)
-'''
-
-class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        
-        last = m + n - 1
-        while m > 0 and n > 0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m -= 1
-            else:
-                nums1[last] = nums2[n-1]
-                n -= 1
-            last -= 1
-        while n >0:
-            nums1[last] = nums2[n-1]
-            last -= 1
-            n -= 1
+1class Solution:
+2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+3        """
+4        Do not return anything, modify nums1 in-place instead.
+5        """
+6        last = m+n-1
+7        while m>0 and n>0:
+8            if nums1[m - 1] > nums2[n - 1]:
+9                nums1[last] = nums1[m - 1]
+10                m -= 1
+11            else:
+12                nums1[last] = nums2[n - 1]
+13                n -= 1
+14            last -= 1
+15        while n>0:
+16            nums1[last] = nums2[n - 1]
+17            n, last = n-1, last-1
+18        return nums1
+19        
