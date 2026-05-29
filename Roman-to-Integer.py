7ea6aaf2afd2fb5e855eae4d8b5bdc4649435da4
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-05-28 17:22:02 -0700

    [13] [Time Beats: 64.43%] [Memory Beats: 58.07%] - LeetPush

--- Roman-to-Integer.py
+++ Roman-to-Integer.py
@@ -1,22 +1,13 @@
 1class Solution:
 2    def romanToInt(self, s: str) -> int:
-3        hashRoman = {'I':1,
-4                    'V':5,
-5                    'X':10,
-6                    'L':50,
-7                    'C':100,
-8                    'D':500,
-9                    'M':1000}
-10
-11        total = 0
-12        for i in range(len(s)-1):
-13            if hashRoman[s[i]] < hashRoman[s[i+1]]:
-14                print(hashRoman[s[i]])
-15                total -= hashRoman[s[i]]
-16                print("total_up = ", total)
-17            else:
-18                print(i, hashRoman[s[i]])
-19                total += hashRoman[s[i]]
-20                print("total = ", total)
-21        total += hashRoman[s[-1]]
-22        return total
+3        s_hash = {
+4            'I': 1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000
+5        }
+6        total = 0
+7        for i in range(len(s)-1):
+8            if s_hash[s[i]] < s_hash[s[i+1]]:
+9                total -= s_hash[s[i]]
+10            else:
+11                total += s_hash[s[i]]
+12        total += s_hash[s[-1]]
+13        return total
