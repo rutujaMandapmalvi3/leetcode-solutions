d67d5c376b854bd027508a8f74837232e8a8f83e
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-06-18 14:57:23 -0700

    [202] [Time Beats: 37.62%] [Memory Beats: 60.90%] - LeetPush

--- Happy-Number.py
+++ Happy-Number.py
@@ -1,20 +1,13 @@
 1class Solution:
 2    def isHappy(self, n: int) -> bool:
-3        visit = set()
-4
-5        while n not in visit:
-6            visit.add(n)
-7            n = self.sqr_n(n)
-8            if n == 1:
-9                return True
-10            
-11        return False
-12    
-13    def sqr_n(self, n):
-14        output = 0
-15        while n:
-16            digit = n % 10
-17            digit = digit ** 2
-18            output += digit
-19            n = n // 10
-20        return output
+3        slow, fast = n, self.sqr_n(n)
+4        while fast != 1 and slow != fast:
+5            slow = self.sqr_n(slow)
+6            fast = self.sqr_n(self.sqr_n(fast))
+7        return fast == 1
+8
+9    def sqr_n(self, x):
+10        total = 0
+11        for i in str(x):
+12            total += int(i) * int(i)
+13        return total
