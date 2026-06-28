b1710fb105fae16fcd23f68c99ea84de38c87716
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-06-28 10:30:55 -0700

    [101] [Time Beats: 100.00%] [Memory Beats: 53.17%] - LeetPush

--- Symmetric-Tree.py
+++ Symmetric-Tree.py
@@ -1,17 +1,15 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-#Time complexity = O(n)
-#space = O(h)
-class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        def isSame(left, right):
-            if not left and not right:
-                return True
-            if not left or not right:
-                return False
-            return (left.val == right.val and isSame(left.left, right.right) and isSame(left.right, right.left))
-        return isSame(root.left, root.right)
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+9        def dfs(left, right):
+10            if not left and not right:
+11                return True
+12            if not left or not right:
+13                return False
+14            return (left.val == right.val and dfs(left.left, right.right) and dfs(left.right, right.left))
+15        return dfs(root.left, root.right)
