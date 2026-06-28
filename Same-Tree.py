f964475abca69ebbb6bb402bb8d23e91ed866f01
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-06-28 00:56:36 -0700

    [100] [Time Beats: 100.00%] [Memory Beats: 30.54%] - LeetPush

--- Same-Tree.py
+++ Same-Tree.py
@@ -1,17 +1,13 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-# Time complexity = O(p+q)
-#Space = O(n)
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
-            return True
-
-        if not p or not q or p.val != q.val:
-            return False
-        
-        return(self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+9        if not p and not q:
+10            return True
+11        if not p or not q or p.val != q.val:
+12            return False
+13        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
