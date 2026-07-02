9868f64a99c0ea48d2f66653a06055ed9d09a89a
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-07-01 21:34:24 -0700

    [121] [Time Beats: 5.19%] [Memory Beats: 74.75%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.py
+++ Best-Time-to-Buy-and-Sell-Stock.py
@@ -1,6 +1,6 @@
 1class Solution:
 2    def maxProfit(self, prices: List[int]) -> int:
-3        l, r = 0, 1
+3        '''l, r = 0, 1
 4        maxP = 0
 5        while r < len(prices):
 6            if prices[l] < prices[r]:
@@ -9,5 +9,14 @@
 9            else:
 10                l = r
 11            r += 1
-12        return maxP
+12        return maxP'''
 13
+14        l, r = 0, 1
+15        maxP = 0
+16        while r < len(prices): 
+17            if prices[r] < prices[l]:
+18                l = r
+19            maxP = max(maxP, prices[r] - prices[l])
+20            r += 1
+21            
+22        return maxP
