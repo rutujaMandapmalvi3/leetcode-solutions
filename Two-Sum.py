676d19f5e91905467771855118d0a061dcc24f06
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-07-01 18:43:38 -0700

    [1] [Time Beats: 100.00%] [Memory Beats: 18.03%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,15 +1,25 @@
 1class Solution:
 2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        '''        for i in range(len(nums)):
+3        '''for i in range(len(nums)):
 4            for j in range(i+1, len(nums)):
 5                if nums[i] + nums[j] == target:
 6                    return [i, j]'''
 7
-8        indices = {}
+8        '''indices = {}
 9        for i, val in enumerate(nums):    
 10            indices[val] = i
 11        for i, val in enumerate(nums):
 12            diff = target - val
 13            if diff in indices and indices[diff] != i:
 14                print(indices[diff], i)
-15                return[indices[diff], i]
+15                return[indices[diff], i]'''
+16
+17        indices = {}
+18        for i, val in enumerate(nums):    
+19            indices[val] = i
+20        l = 0
+21        while l < len(nums):
+22            diff = target - nums[l]
+23            if diff in indices and indices[diff] != l:
+24                return[indices[diff], l]
+25            l += 1
