dba9277cc8c08bb044ca6e7d4b8c3053b180af0e
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-07-02 20:01:37 -0700

    [875] [Time Beats: 48.75%] [Memory Beats: 79.77%] - LeetPush

---
+++ 875-Koko-Eating-Bananas.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+        while l <= r:
+            mid = (l + r) // 2
+            hours = 0
+            for i in piles:
+                hours += math.ceil(i / mid)
+            if hours <= h:
+                res = min(res, mid)
+                r = mid - 1
+            else:
+                l = mid + 1
+        return res
