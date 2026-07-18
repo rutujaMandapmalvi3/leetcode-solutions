dfa0473404faa20ad4b0d2c7fdeedec38e341a81
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-07-18 16:44:40 -0700

    [104] [Time Beats: 100.00%] [Memory Beats: 6.77%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.py
+++ 104-Maximum-Depth-of-Binary-Tree.py
@@ -1,67 +1,32 @@
-#----------------------------------
-'''
-recursion dfs -
-time complexity = o(n) 
-space complexity = O(h) -> height of the tree
-
-iteration dfs -
-time complexity = o(n) 
-space complexity = O(h) -> height of the tree
-
-iteration bfs -
-time complexity = o(n)
-space complexity = O(w) -> max width of the tree
-'''
-#----------------------------------
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
-#recursive dfs
-'''
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''
         if not root:
             return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))'''
 
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-'''
-
-#iterative dfs
-class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
-        if not root:
+        '''if not root:
             return 0
-        stack = [[root, 1]]
+        stk = [[root, 1]]
         res = 1
-        while stack:
-            node, depth = stack.pop()
+        while stk:
+            node, depth = stk.pop()
             if node:
                 res = max(res, depth)
-                stack.append([node.left, depth +1])
-                stack.append([node.right, depth+1])
-        return res
+                stk.append([node.right, depth + 1])
+                stk.append([node.left, depth + 1])
+        return res'''
 
-
-#iterative bfs
-    #traverse level by level
-'''
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        level = 0
-        q = deque([root])
-        while q:
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
-'''
+
+        left_max = self.maxDepth(root.left)
+        right_max = self.maxDepth(root.right)
+        
+        return 1 + max(left_max, right_max)
