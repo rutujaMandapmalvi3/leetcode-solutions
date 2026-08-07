ab7686a1a48defea212b750c5c2f2c553de3b62e
RUTUJA MANDAPMALVI <mandapmalvirutuja@gmail.com>
2026-08-07 13:54:20 -0700

    [94] [Time Beats: 100.00%] [Memory Beats: 27.21%] - LeetPush

--- 94-Binary-Tree-Inorder-Traversal.py
+++ 94-Binary-Tree-Inorder-Traversal.py
@@ -1,17 +1,3 @@
-#----------------------------------
-'''
-recursion -
-time complexity = o(n) -> we check for each node
-space complexity = O(n)
-
-iteration -
-time complexity = o(n) -> stack and another list - o(n)
-space complexity = O(n)
-'''
-#----------------------------------
-
-# inorder traversal: left -> main node -> right
-#---------------------------------
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -20,27 +6,19 @@ space complexity = O(n)
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # recursive - nested function - o(n)
-        '''res = []
-        def inorder(root):
-            if not root:
+        '''stack = []
+        def dfs(node):
+            if not node:
                 return
-            inorder(root.left)
-            res.append(root.val)
-            inorder(root.right)
-        inorder(root)
-        return res'''
 
-        # iterative - stack and another list - o(n)
-        res = []
-        stack = []
-        curr = root
+            dfs(node.left)
+            stack.append(node.val)
+            dfs(node.right)
 
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()
-            res.append(curr.val)
-            curr = curr.right
-        return res
+        dfs(root)
+
+        return stack
+        '''
+        if not root:
+            return []
+        return (self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right))
